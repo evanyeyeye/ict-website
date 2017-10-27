f663c982b3ed54ec656567105e80bc90fbcb16af
Evan Shi <evanyshi@gmail.com>
2017-10-26 19:32:14 -0500

    fix time zone

--- contests.php
+++ contests.php
@@ -10,8 +10,9 @@ if (!class_exists('Template')) {
 
 <?php
 
+date_default_timezone_set('America/New_York');
 // if (strtotime("2017:10:27 14:30:00") <= strtotime("now")) {
-if (strtotime("2017:10:26 20:25:00") <= strtotime("now")) {
+if (strtotime("2017:10:26 20:33:00") <= strtotime("now")) {
     echo '<div class="notification"><a href="contest1.zip">In-House Contest 1</a></div>';
 } else {
     echo '<div class="notification"><p>Stay tuned!</p></div>';
