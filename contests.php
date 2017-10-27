908a80da73b0637f221409f66d5882bfa74a1de3
Evan Shi <evanyshi@gmail.com>
2017-10-27 18:12:42 -0500

    add contest 1 solutions

--- contests.php
+++ contests.php
@@ -8,14 +8,12 @@ if (!class_exists('Template')) {
 
 ?>
 
+<div class="notification">
+    <a href="contest1.zip">In-House Contest 1</a>
+</div>
+
 <?php
 
 date_default_timezone_set('America/New_York');
-
-if (strtotime("2017:10:27 14:26:00") <= strtotime("now")) {
-    echo '<div class="notification"><a href="contest1.zip">In-House Contest 1</a></div>';
-} else {
-    echo '<div class="notification"><p>Stay tuned!</p></div>';
-}
 
 ?>
