39e38a1a2ac17dd3bac5f083be965982dd87cfc4
Evan Shi <evanyshi@gmail.com>
2017-10-26 19:20:22 -0500

    test time

--- contests.php
+++ contests.php
@@ -8,6 +8,13 @@ if (!class_exists('Template')) {
 
 ?>
 
-<div class="notification">
-	<p>Stay tuned!</p>
-</div>
+<?php
+
+// if (strtotime("2017:10:27 14:30:00") <= strtotime("now")) {
+if (strtotime("2017:10:26 20:25:00") <= strtotime("now")) {
+    echo '<div class="notification"><a href="contest1.zip">In-House Contest 1</a></div>';
+} else {
+    echo '<div class="notification"><p>Stay tuned!</p></div>';
+}
+
+?>
